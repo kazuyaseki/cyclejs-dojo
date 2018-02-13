ff600786fd377b2730721842798ded4e4a63d2f7
kazuya-seki <track613armory@gmail.com>
2018-02-13 12:21:40 +0900

    cleaned up everything so far

--- basic/script.js
+++ basic/script.js
@@ -1,51 +1,12 @@
 import xs from "xstream";
 import fromEvent from "xstream/extra/fromEvent";
-import { h, h1, span, makeDOMDriver } from "@cycle/dom";
+import { makeDOMDriver } from "@cycle/dom";
 import { run } from "@cycle/run";
 
 const main = sources => {
-  const click$ = sources.DOM.select("span").events("click");
-  return {
-    DOM: click$
-      .startWith(null)
-      .map(() => xs.periodic(1000).fold(prev => prev + 1, 0))
-      .flatten()
-      .map(i =>
-        h1({ style: { backgroundColor: "red" } }, [
-          span([`Seconds elapsed: ${i}`])
-        ])
-      ),
-    log: xs.periodic(2000).fold(prev => prev + 1, 0)
-  };
-};
-
-const logDriver = msg$ => {
-  msg$.subscribe({
-    next: msg => {
-      console.log(msg);
-    }
-  });
-};
-
-const run = (mainFn, drivers) => {
-  const fakeSinks = {};
-  Object.keys(drivers).forEach(key => {
-    fakeSinks[key] = xs.create();
-  });
-
-  const sources = {};
-  Object.keys(drivers).forEach(key => {
-    sources[key] = drivers[key](fakeSinks[key]);
-  });
-
-  const sinks = mainFn(sources);
-
-  Object.keys(sinks).forEach(key => {
-    fakeSinks[key].imitate(sinks[key]);
-  });
+  return {};
 };
 
 run(main, {
-  DOM: makeDOMDriver("#app"),
-  log: logDriver
+  DOM: makeDOMDriver("#app")
 });
