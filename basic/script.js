5bc8eb8d81c49a3129151b721205be758794d4a9
kazuya-seki <track613armory@gmail.com>
2018-02-13 12:32:45 +0900

    learned how to use fold function to accumulate state

--- basic/script.js
+++ basic/script.js
@@ -1,10 +1,27 @@
 import xs from "xstream";
-import fromEvent from "xstream/extra/fromEvent";
-import { makeDOMDriver } from "@cycle/dom";
+import { button, p, label, div, makeDOMDriver } from "@cycle/dom";
 import { run } from "@cycle/run";
 
 const main = sources => {
-  return {};
+  const decClick$ = sources.DOM.select(".dec").events("click");
+  const incClick$ = sources.DOM.select(".inc").events("click");
+
+  const dec$ = decClick$.map(() => -1);
+  const inc$ = incClick$.map(() => +1);
+
+  const delta$ = xs.merge(dec$, inc$);
+
+  const number$ = delta$.fold((prev, x) => prev + x, 0);
+
+  return {
+    DOM: number$.map(number =>
+      div([
+        button(".dec", "Decrement"),
+        button(".inc", "Increment"),
+        p([label("Counr: " + number)])
+      ])
+    )
+  };
 };
 
 run(main, {
