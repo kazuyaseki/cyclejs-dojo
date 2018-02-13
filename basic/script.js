b1d51519e418d77e41de88990626ebb6faa916c4
kazuya-seki <track613armory@gmail.com>
2018-02-13 12:25:31 +0900

    added reactive input field

--- basic/script.js
+++ basic/script.js
@@ -1,10 +1,22 @@
 import xs from "xstream";
 import fromEvent from "xstream/extra/fromEvent";
-import { makeDOMDriver } from "@cycle/dom";
+import { div, label, input, hr, h1, makeDOMDriver } from "@cycle/dom";
 import { run } from "@cycle/run";
 
 const main = sources => {
-  return {};
+  const inputEv$ = sources.DOM.select(".field").events("input");
+  const name$ = inputEv$.map(ev => ev.target.value).startWith("");
+
+  return {
+    DOM: name$.map(name =>
+      div([
+        label(["Name:"]),
+        input(".field", { attrs: { type: "text" } }),
+        hr(),
+        h1("Hello " + name + "!")
+      ])
+    )
+  };
 };
 
 run(main, {
