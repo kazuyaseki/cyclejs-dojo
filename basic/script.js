552a59147ff4fd87f42aaf6345f089d3ae994fe9
kazuya-seki <track613armory@gmail.com>
2018-02-13 12:30:51 +0900

    cleaned up again

--- basic/script.js
+++ basic/script.js
@@ -1,22 +1,10 @@
 import xs from "xstream";
 import fromEvent from "xstream/extra/fromEvent";
-import { div, label, input, hr, h1, makeDOMDriver } from "@cycle/dom";
+import { makeDOMDriver } from "@cycle/dom";
 import { run } from "@cycle/run";
 
 const main = sources => {
-  const inputEv$ = sources.DOM.select(".field").events("input");
-  const name$ = inputEv$.map(ev => ev.target.value).startWith("");
-
-  return {
-    DOM: name$.map(name =>
-      div([
-        label(["Name:"]),
-        input(".field", { attrs: { type: "text" } }),
-        hr(),
-        h1("Hello " + name + "!")
-      ])
-    )
-  };
+  return {};
 };
 
 run(main, {
