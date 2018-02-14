b6d9284a991019d96509879f8414008107bccd4b
kazuya-seki <track613armory@gmail.com>
2018-02-14 10:45:15 +0900

    cleaned up as usual

--- basic/script.js
+++ basic/script.js
@@ -5,40 +5,13 @@ import { run } from "@cycle/run";
 import { html } from "snabbdom-jsx";
 
 const main = sources => {
-  const click$ = sources.DOM.select(".get-first").events("click");
-
-  const request$ = click$.map(ev => ({
-    url: "https://jsonplaceholder.typicode.com/users/1",
-    method: "GET",
-    category: "user-data"
-  }));
-
-  const response$ = sources.HTTP.select("user-data")
-    .flatten()
-    .map(res => res.body);
-
-  const vdom$ = response$.startWith({}).map(response => (
-    <div>
-      <button className="get-first">Get the first User</button>
-      <div className="user-details">
-        <h1 className="user-name">{response.name}</h1>
-        <h4 className="user-email">{response.email}</h4>
-        <a className="user-website" href={response.website}>
-          {response.website}
-        </a>
-      </div>
-    </div>
-  ));
-
   return {
-    DOM: vdom$,
-    HTTP: request$
+    DOM: vdom$
   };
 };
 
 const drivers = {
-  DOM: makeDOMDriver("#app"),
-  HTTP: makeHTTPDriver()
+  DOM: makeDOMDriver("#app")
 };
 
 run(main, drivers);
